3971e909dc517bae375858a1235341426a75863a
Likhith-18 <vparvathi222@gmail.com>
2024-12-17 17:44:07 +0530

    Simplify ChatBot API URL logging and iframe source reference

--- client/src/Components/ChatBot.jsx
+++ client/src/Components/ChatBot.jsx
@@ -1,13 +1,13 @@
 const ChatBot = () => {
   const api = import.meta.env.VITE_AZURE_WEB_APP_URL;
-  console.log(`S${api}/chatbot`);
+  console.log(api);
 
   return (
     <div style={{ width: "100vw", height: "100vh", margin: 0 }}>
       <iframe
         // src="http://localhost:8000/chatbot"
         // src="https://ayurvision-server.onrender.com/chatbot/"
-        src={`${api}/chatbot`}
+        src={api}
         width="100%"
         height="94%"
         style={{ border: "none" }}
